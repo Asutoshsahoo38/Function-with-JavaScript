bd73437f02ad1e5f10a2753a7f82222cdea059cb
Asutoshsahoo38 <96715429+Asutoshsahoo38@users.noreply.github.com>
2022-09-15 22:12:42 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,23 @@
+// function with argument 
+function avgofnumber(x,y){
+    return 1 + (x+y)/2
+    
+  }
+  a = 5
+  b = 10
+  c = 11
+  d = 12
+  console.log(avgofnumber(a,b))
+  console.log(avgofnumber(c,b))
+  console.log(avgofnumber(a,c))
+  console.log("Done")
+  // Another way to use function 
+  const sum = (s,d)=>{
+    return s+d
+  }
+  console.log(sum(a,b))
+  // Another way to use function
+  const add = ()=>{
+    return a+b
+  }
+  console.log(add(a,b))
